b2fb5adfab9c9588042d6b1cb4e5b26151693830
Spock Nah <snah@riotgames.com>
2019-03-20 22:04:47 +0900

    Update maven-command.sh

--- maven-command.sh
+++ maven-command.sh
@@ -1,1 +1,4 @@
 mvn archetype:generate -DgroupId=com.cnu.realcoding -DartifactId=my-app -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+
+mvn exec:java -Dexec.mainClass="com.cnu.realcoding.App"
+
